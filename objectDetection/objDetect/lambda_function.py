bcd51850bd7bc1825528ed444a644ffbe29e0475
t109598031 <80373576+t109598031@users.noreply.github.com>
2021-04-15 17:57:56 +0800

    Update lambda_function.py

--- objectDetection/objDetect/lambda_function.py
+++ objectDetection/objDetect/lambda_function.py
@@ -10,6 +10,6 @@ def lambda_handler(event, context):
     objDetection = ObjDetection(dataModel, config.aws_access_key_id, config.aws_secret_access_key, config.region_name)
     objDetection.objDetect(config.objectBucketName)
     objDetection.storeImage(config.bucketName)
-    #objDetection.modifyDataModel()
+    objDetection.modifyDataModel()
     
     return objDetection.getModel()
