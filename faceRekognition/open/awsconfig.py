b07f5d6f447c40eb3163a15f6a1d1f71dc4c6e68
BananaDuncan <b10121014@gmail.com>
2021-04-15 19:38:33 +0800

    Add files via upload

---
+++ faceRekognition/open/awsconfig.py
@@ -0,0 +1,4 @@
+access_key = ''
+secret_access_key = ''
+region_name = ''
+stepfunction_ARN = 'your ARN'
---
+++ faceRekognition/open/config.py
@@ -0,0 +1,10 @@
+config = {
+	"rekognize"{
+		"mode":0,
+		"register":True
+	}
+    "threshold":70,
+    "s3Bucket":"secomv3-source-image"
+}
+
+stepFunctionActivateFreqency = 1
