727e72e8b1e7784f82f08db3c581ac5f2dda5adc
BananaDuncan <b10121014@gmail.com>
2021-04-15 18:15:42 +0800

    Add files via upload

---
+++ objectDetection/local/open/config.py
@@ -0,0 +1,15 @@
+config = {
+    "stepFunctionActivateFreqency":1,
+    "cameraCoveredThreshold":30,
+    "cameraMovedThreshold":30,
+    "objectLongLostThreshold":10,
+    "objectShortLostThreshold":30,
+    "shortLostObjectList":["Chair"],
+    "objectLongStayThreshold":10,
+    "objectShortStayThreshold":30,
+    "shortStayObjectList":["Box"],
+    "seceneOffsetThreshold":0.2,
+    "alertNotifyActivation":True,
+    "initObject":True
+}
+stepFunctionActivateFreqency = 1
